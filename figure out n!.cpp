60f61942f882c135a5a470018ad4efbd5dbfbcdd
dlmagkub <luxuary510@gmail.com>
2023-01-07 17:12:53 +0800

    Update figure out n!.cpp

--- figure out n!.cpp
+++ figure out n!.cpp
@@ -1,36 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//int main()
-//{
-//	int i = 0;
-//	for (i = 0; i <= 100; i++)
-//	if(1==i%2)
-//		printf("%d ", i);
-//	else printf(" ");
-//	return 0;
-//}
-
-//int main()
-//{
-//	int a = 0;
-//	a = getchar();
-//		printf("%d %c", a, a);//²éÑ¯ASCIIÂë³ÌÐò
-//	return 0;
-//
-//}
-
-//int main()
-//{
-//	int a = 72;
-//	int b = 69;
-//	int c = 76;
-//	int d = 76;
-//	int e = 79;
-//	printf("%c %c %c %c %c", a, b, c, d, e); //HELLO 72 69 76 79
-//	return 0;
-//}
-
 int main()
 {
 	int n = 0;
