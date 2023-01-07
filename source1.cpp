53e55a11050ce28dad1807a6d729699b5c916c04
dlmagkub <luxuary510@gmail.com>
2023-01-07 12:22:19 +0800

    Update source1.cpp

--- source1.cpp
+++ source1.cpp
@@ -1,12 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100; i++)
+//	if(1==i%2)
+//		printf("%d ", i);
+//	else printf(" ");
+//	return 0;
+//}
+
 int main()
 {
-	int i = 0;
-	for (i = 0; i <= 100; i++)
-	if(1==i%2)
-		printf("%d ", i);
-	else printf(" ");
+	int a = 0;
+	a = getchar();
+		printf("%d %c", a, a);//²éÑ¯ASCIIÂë³ÌÐò
 	return 0;
+
 }
